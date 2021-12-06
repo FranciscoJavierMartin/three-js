315d379709691a1be90143f1b9051a1452dfadbf
Francisco <javiermartincuenca@gmail.com>
2021-12-06 19:32:14 +0100

    wip

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,36 +1,31 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import Stats from 'three/examples/jsm/libs/stats.module';
+import { GUI } from 'dat.gui';
 
 const scene = new THREE.Scene();
+scene.add(new THREE.AxesHelper(5));
 
-const camera1 = new THREE.PerspectiveCamera(75, 1, 0.1, 10);
-const camera2 = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 10);
-const camera3 = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 10);
-const camera4 = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 10);
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  10
+);
 
-camera1.position.z = 2;
-camera1.position.y = 1;
-camera2.lookAt(new THREE.Vector3(0, 0, 0));
-camera3.position.z = 1;
-camera4.position.x = 1;
-camera4.lookAt(new THREE.Vector3(0, 0, 0));
+camera.position.x = 4;
+camera.position.y = 4;
+camera.position.z = 4;
 
-const canvas1 = document.getElementById('c1') as HTMLCanvasElement;
-const canvas2 = document.getElementById('c2') as HTMLCanvasElement;
-const canvas3 = document.getElementById('c3') as HTMLCanvasElement;
-const canvas4 = document.getElementById('c4') as HTMLCanvasElement;
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-const renderer1 = new THREE.WebGLRenderer({ canvas: canvas1 });
-renderer1.setSize(200, 200);
-const renderer2 = new THREE.WebGLRenderer({ canvas: canvas2 });
-renderer2.setSize(200, 200);
-const renderer3 = new THREE.WebGLRenderer({ canvas: canvas3 });
-renderer3.setSize(200, 200);
-const renderer4 = new THREE.WebGLRenderer({ canvas: canvas4 });
-renderer4.setSize(200, 200);
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.target.set(8,0,0)
+// controls.addEventListener('change', render);
 
-new OrbitControls(camera1, renderer1.domElement);
-
+const light1 = new THREE.PointLight()
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({
   color: 0x00ff00,
@@ -40,20 +35,53 @@ const material = new THREE.MeshBasicMaterial({
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-function animate(): void {
-  requestAnimationFrame(animate);
-
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
+window.addEventListener('resize', onWindowResize, false);
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
   render();
 }
 
+const stats = Stats();
+document.body.appendChild(stats.dom);
+
+const gui = new GUI();
+const cubeFolder = gui.addFolder('Cube');
+const cubeRotationFolder = cubeFolder.addFolder('Rotation');
+cubeFolder.add(cube.rotation, 'x', 0, Math.PI * 2);
+cubeFolder.add(cube.rotation, 'y', 0, Math.PI * 2);
+cubeFolder.add(cube.rotation, 'z', 0, Math.PI * 2);
+cubeFolder.open();
+cubeRotationFolder.open();
+const cubePositionFolder = cubeFolder.addFolder('Position');
+cubePositionFolder.add(cube.position, 'x', -10, 10, 2);
+cubePositionFolder.add(cube.position, 'y', -10, 10, 2);
+cubePositionFolder.add(cube.position, 'z', -10, 10, 2);
+cubeFolder.open();
+cubePositionFolder.open();
+const cubeScaleFolder = cubeFolder.addFolder('Scale');
+cubeScaleFolder.add(cube.scale, 'x', -5, 5);
+const cameraFolder = gui.addFolder('Camera');
+cameraFolder.add(camera.position, 'z', 0, 10);
+cameraFolder.open();
+
+function animate(): void {
+  requestAnimationFrame(animate);
+
+  // stats.begin();
+  // cube.rotation.x += 0.01;
+  // cube.rotation.y += 0.01;
+  // stats.end();
+
+  render();
+
+  stats.update();
+}
+
 function render(): void {
-  renderer1.render(scene, camera1);
-  renderer2.render(scene, camera2);
-  renderer3.render(scene, camera3);
-  renderer4.render(scene, camera4);
+  renderer.render(scene, camera);
 }
 
 animate();
+// render()
